ed27e5b8c68af281e4d7c54e433036bd0003ea33
Johan <johanwiren1@gmail.com>
2020-02-04 23:28:36 +0100

    added node server for hosting

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const port = 3020
+const app = express();
+app.use(express.static(__dirname + "/"));
+app.get('*', function(req, res) {
+  res.sendfile(__dirname + "/index.html");
+});
+app.listen(port);
+console.log("listening on port", port);
