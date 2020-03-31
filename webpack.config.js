f9a13426ccb3847531d816ff5d9b3dea61b3ee87
Johan <johanwiren1@gmail.com>
2020-03-31 11:46:55 +0200

    another bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
+  devtool: 'source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -15,5 +17,17 @@ module.exports = {
       inject: true,
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
-  ]
+  ],
+  optimization: {
+    minimizer: [
+        new TerserPlugin({
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        parallel: true,
+        // Enable file caching
+        cache: true,
+        sourceMap: true,
+      }),
+    ],
+  },
 }
